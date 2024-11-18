28badf148a3955f0ae32bd7e208b554d0ce45be1
miracleonyenma <miracleficient@gmail.com>
2024-11-18 11:55:09 +0100

    updates: styling changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <section className="site-section">
-        <div className="wrapper">
-          <h1 className="text-center text-5xl font-bold">Welcome to Starter</h1>
-          <p className="text-center">
-            A platform for developers to build and share their projects.
+        <div className="wrapper flex flex-col items-center gap-2">
+          <h1 className="text-center text-5xl font-bold lg:text-7xl">
+            Keep track of issues, find solutions faster.
+          </h1>
+          <p className="mx-auto text-center text-lg lg:w-4/5 lg:text-xl">
+            Get updates on issues that arise, find solutions with all the help
+            you can get and keep accessible for the next person.
           </p>
+          <Link href={"/auth/register"} className="btn primary lg">
+            Get Started
+          </Link>
         </div>
       </section>
     </main>
--- components/Site/Logo.tsx
+++ components/Site/Logo.tsx
@@ -2,9 +2,9 @@ import StarterIcon from "@/components/Icon/Starter";
 
 const SiteLogo = () => {
   return (
-    <figure className="flex items-center gap-1">
+    <figure className="flex items-center gap-1 text-green-600">
       <StarterIcon className="icon text-payred-06 h-12 w-12" />
-      <figcaption className="text-2xl font-extrabold">Starter</figcaption>
+      <figcaption className="mt-1 text-2xl font-extrabold">Pancea</figcaption>
     </figure>
   );
 };
