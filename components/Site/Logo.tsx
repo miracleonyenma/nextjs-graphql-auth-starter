cde8c9ec1a28a0f40658a8471d041e8ebc8a57af
miracleonyenma <miracleficient@gmail.com>
2024-12-30 13:18:29 +0100

    refactor: update StarterIcon dimensions and color, modify SiteLogo text and styling

--- components/Site/Logo.tsx
+++ components/Site/Logo.tsx
@@ -2,9 +2,9 @@ import StarterIcon from "@/components/Icon/Starter";
 
 const SiteLogo = () => {
   return (
-    <figure className="flex items-center gap-1 text-indigo-600 dark:text-indigo-400">
-      <StarterIcon className="icon text-payred-06 h-12 w-12" />
-      <figcaption className="mt-1 text-2xl font-extrabold">Starter</figcaption>
+    <figure className="flex items-center gap-1 text-indigo-400 dark:text-indigo-400">
+      {/* <StarterIcon className="icon text-payred-06 h-12 w-12" /> */}
+      <figcaption className="mt-1 text-2xl font-extrabold">memoire</figcaption>
     </figure>
   );
 };
